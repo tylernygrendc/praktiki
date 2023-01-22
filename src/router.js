c31b266ebeae7e8e46a60fef04b6db26ed443d6f
Tyler Nygren <tylernygren@Tylers-MBP.lan>
2023-01-22 14:13:55 -0500

    update

--- src/router.js
+++ src/router.js
@@ -8,11 +8,38 @@ const router = express();
 
 router.set('view engine', 'pug');
 
+router.get('/auth/chat', (req, res) => {
+    res.status(200)
+        .render("chat", {
+            title: "Chat",
+            asset: "chat",
+            description: ""
+        });
+});
+
 router.get('/auth/files', (req, res) => {
     res.status(200)
         .render("files", {
             title: "Files",
             asset: "files",
+            description: ""
+        });
+});
+
+router.get('/auth/schedule', (req, res) => {
+    res.status(200)
+        .render("schedule", {
+            title: "Schedule",
+            asset: "schedule",
+            description: ""
+        });
+});
+
+router.get('/auth/terminal', (req, res) => {
+    res.status(200)
+        .render("terminal", {
+            title: "Terminal",
+            asset: "terminal",
             description: ""
         });
 });
