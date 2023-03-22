971fa874ba6ecf7d4de21373c3c5a93298f316de
Tyler Nygren <tylernygren@gmail.com>
2023-03-22 12:46:34 -0400

    update

--- src/router.js
+++ src/router.js
@@ -8,39 +8,17 @@ const router = express();
 
 router.set('view engine', 'pug');
 
-router.get('/auth/chat', (req, res) => {
+router.get('/auth/file', (req, res) => {
     res.status(200)
-        .render("chat", {
-            title: "Chat",
-            asset: "chat",
-            description: ""
-        });
-});
-
-router.get('/auth/files', (req, res) => {
-    res.status(200)
-        .render("files", {
-            title: "Files",
-            asset: "files",
-            description: ""
+        .render("file", {
+            page: "file"
         });
 });
 
 router.get('/auth/schedule', (req, res) => {
     res.status(200)
         .render("schedule", {
-            title: "Schedule",
-            asset: "schedule",
-            description: ""
-        });
-});
-
-router.get('/auth/terminal', (req, res) => {
-    res.status(200)
-        .render("terminal", {
-            title: "Terminal",
-            asset: "terminal",
-            description: ""
+            page: "schedule"
         });
 });
 
