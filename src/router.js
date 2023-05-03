6bca4ead79baf8cfbc02f13165944b27ce1d78cc
Tyler Nygren <tylernygren@gmail.com>
2023-05-03 06:35:47 -0400

    update

--- src/router.js
+++ src/router.js
@@ -8,6 +8,20 @@ const router = express();
 
 router.set('view engine', 'pug');
 
+router.get('/', (req, res) => {
+    res.status(200)
+        .render("product", {
+            page: "product"
+        });
+});
+
+router.get('/auth', (req,res) => {
+    res.status(200)
+        .render("auth", {
+            page: "auth"
+        });
+});
+
 router.get('/auth/file', (req, res) => {
     res.status(200)
         .render("file", {
@@ -19,6 +33,41 @@ router.get('/auth/schedule', (req, res) => {
     res.status(200)
         .render("schedule", {
             page: "schedule"
+        });
+});
+
+router.get('/auth/billing', (req, res) => {
+    res.status(200)
+        .render("billing", {
+            page: "billing"
+        });
+});
+
+router.get('/auth/business', (req, res) => {
+    res.status(200)
+        .render("business", {
+            page: "business"
+        });
+});
+
+router.get('/auth/tasks', (req, res) => {
+    res.status(200)
+        .render("tasks", {
+            page: "tasks"
+        });
+});
+
+router.get('/auth/chat', (req, res) => {
+    res.status(200)
+        .render("chat", {
+            page: "chat"
+        });
+});
+
+router.get('/auth/account', (req, res) => {
+    res.status(200)
+        .render("account", {
+            page: "account"
         });
 });
 
