de7fcefa7eb9af05c17e0ad448a46a674a41eed8
Tyler Nygren <tylernygren@gmail.com>
2023-06-06 16:17:19 -0400

    update

--- src/router.js
+++ src/router.js
@@ -10,64 +10,57 @@ router.set('view engine', 'pug');
 
 router.get('/', (req, res) => {
     res.status(200)
-        .render("productApp", {
-            page: "product app"
-        });
-});
-
-router.get('/auth-app', (req,res) => {
-    res.status(200)
-        .render("authApp", {
-            page: "authApp"
+        .render("productPage", {
+            page: "Login or Sign Up"
         });
 });
 
 router.get('/auth/file-app', (req, res) => {
     res.status(200)
         .render("fileApp", {
-            page: "fileApp"
+            page: "File"
         });
 });
 
 router.get('/auth/schedule-app', (req, res) => {
     res.status(200)
         .render("scheduleApp", {
-            page: "scheduleApp"
+            page: "Schedule"
         });
 });
 
 router.get('/auth/billing-app', (req, res) => {
     res.status(200)
         .render("billingApp", {
-            page: "billingApp"
+            page: "Billing"
         });
 });
 
 router.get('/auth/business-app', (req, res) => {
     res.status(200)
         .render("businessApp", {
-            page: "businessApp"
+            page: "Business"
         });
 });
 
 router.get('/auth/task-app', (req, res) => {
     res.status(200)
         .render("taskApp", {
-            page: "taskApp"
+            page: "Task"
         });
 });
 
 router.get('/auth/chat-app', (req, res) => {
     res.status(200)
         .render("chatApp", {
-            page: "chatApp"
+            page: "Chat"
         });
 });
 
 router.get('/auth/account-app', (req, res) => {
     res.status(200)
         .render("account", {
-            page: "account"
+            page: "Account"
         });
 });
 
