689a8b040402b72e51b4fa2857ed30665e3c23c1
Tyler Nygren <tylernygren@gmail.com>
2023-11-28 23:48:20 -0500

    update

--- src/router.js
+++ src/router.js
@@ -5,59 +5,11 @@ const router = express();
 
 router.set('view engine', 'pug');
 
-router.get('/auth/file-app', (req, res) => {
-    res.status(200)
-        .render("fileApp", {
-            title: "File",
-            page: "fileApp"
-        });
-});
-
-router.get('/auth/schedule-app', (req, res) => {
-    res.status(200)
-        .render("scheduleApp", {
-            title: "Schedule",
-            page: "scheduleApp"
-        });
-});
-
-router.get('/auth/billing-app', (req, res) => {
-    res.status(200)
-        .render("billingApp", {
-            title: "Billing",
-            page: "billingApp"
-        });
-});
-
-router.get('/auth/business-app', (req, res) => {
-    res.status(200)
-        .render("businessApp", {
-            title: "Business",
-            page: "businessApp"
-        });
-});
-
-router.get('/auth/task-app', (req, res) => {
-    res.status(200)
-        .render("taskApp", {
-            title: "Task",
-            page: "taskApp"
-        });
-});
-
-router.get('/auth/chat-app', (req, res) => {
-    res.status(200)
-        .render("chatApp", {
-            title: "Chat",
-            page: "chatApp"
-        });
-});
-
 router.get('/', (req, res) => {
     res.status(200)
-        .render("productPage", {
-            title: "Login or Sign Up",
-            page: "productPage"
+        .render("main", {
+            title: "Praktiki",
+            page: "main"
         });
 });
 
