38051c94a09e12b15672e0250525547341f818ea
tylernygrendc <tylernygren@gmail.com>
2024-01-18 23:35:59 -0500

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-import { autoResizeTextArea, enableNavigation, enableTabs } from "./_ui.js";
+import { autoResizeTextArea, enableNavigation, enableTabs } from "../script_modules/_ui.mjs";
 
 window.addEventListener("load", () => {
     autoResizeTextArea();
